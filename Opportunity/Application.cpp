3b85a868291e5584cf83aa227a7c33e23533cc0b
Martin Pospisil <miiyukiikazuya@gmail.com>
2024-11-15 15:29:15 +0100

    Day Twelve - Starting Point

--- Opportunity/Application.cpp
+++ Opportunity/Application.cpp
@@ -178,7 +178,7 @@ int main()
 		shader.setMat4("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		model = glm::mat4(1.0f);
-		model = glm::scale(model, glm::vec3(2.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
 		shader.setMat4("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
